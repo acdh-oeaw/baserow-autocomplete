1e7ed1bca47af243f818b4b1d2e43fa000a8e02c
csae8092 <peter.andorfer@oeaw.ac.at>
2023-02-23 12:04:17 +0100

    return search results in different formats

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI, HTTPException, Request
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
@@ -5,7 +6,7 @@ from fastapi_cache.decorator import cache
 
 from acdh_baserow_pyutils import BaseRowClient
 from app.config import DATABASES, BASEROW_PW, BASEROW_USER, BASEROW_URL
-from app.utils import make_ac_uris
+from app.utils import make_ac_uris, populate_baserow_response
 
 app = FastAPI()
 
@@ -17,7 +18,10 @@ async def root(request: Request):
             "db_name": x["db_name"],
             "db_id": db_key,
             "endpoint": f"{request.url._url}db/{db_key}/tables",
-            "autocompletes": [make_ac_uris(db_key, key, request.url._url) for key, value in x["endpoints"].items()]
+            "autocompletes": [
+                make_ac_uris(db_key, key, request.url._url)
+                for key, value in x["endpoints"].items()
+            ],
         }
         for db_key, x in DATABASES.items()
     ]
@@ -30,7 +34,9 @@ async def root(request: Request):
 
 
 @app.get("/ac/{db_id}/{endpoint}")
-async def query_endpoint(db_id: str, endpoint: str, q: str):
+async def query_endpoint(
+    db_id: str, endpoint: str, q: str, format: Union[str, None] = "teicompleter"
+):
     try:
         cur_db = DATABASES[db_id]
     except KeyError:
@@ -39,8 +45,11 @@ async def query_endpoint(db_id: str, endpoint: str, q: str):
     cur_conf = cur_db["endpoints"][endpoint]
     br_table_id = cur_conf["table_id"]
     query_field_id = cur_conf["search_field_id"]
-    client = BaseRowClient(BASEROW_USER, BASEROW_PW, "db_token", br_base_url=BASEROW_URL)
-    result = client.search_rows(br_table_id, q, query_field_id, lookup_type="contains")
+    client = BaseRowClient(
+        BASEROW_USER, BASEROW_PW, "db_token", br_base_url=BASEROW_URL
+    )
+    data = client.search_rows(br_table_id, q, query_field_id, lookup_type="contains")
+    result = populate_baserow_response(cur_conf, data, format=format)
     return result
 
 
@@ -53,7 +62,9 @@ async def list_tables(request: Request, db_id: str):
     except KeyError:
         detail_msg = f"no baserow database with ID: <{db_id}> defined in config.py"
         raise HTTPException(status_code=404, detail=detail_msg)
-    client = BaseRowClient(BASEROW_USER, BASEROW_PW, "db_token", br_base_url=BASEROW_URL)
+    client = BaseRowClient(
+        BASEROW_USER, BASEROW_PW, "db_token", br_base_url=BASEROW_URL
+    )
     tables = client.list_tables(cur_db["db_id"])
     for x in tables:
         item = x
@@ -65,7 +76,9 @@ async def list_tables(request: Request, db_id: str):
 @app.get("/db/{db_id}/tables/{table_id}/fields")
 @cache(expire=60 * 60)
 async def list_ac_fields(db_id: str, table_id: str):
-    client = BaseRowClient(BASEROW_USER, BASEROW_PW, "db_token", br_base_url=BASEROW_URL)
+    client = BaseRowClient(
+        BASEROW_USER, BASEROW_PW, "db_token", br_base_url=BASEROW_URL
+    )
     result = client.list_fields(table_id)
     return result
 
--- app/utils.py
+++ app/utils.py
@@ -1,3 +1,30 @@
 def make_ac_uris(db_id: str, endpoint: str, base_uri: str) -> str:
     uri = f"{base_uri}ac/{db_id}/{endpoint}"
     return uri
+
+
+def populate_baserow_response(
+    cur_config: dict, data: dict, format: str = "teicompleter"
+) -> dict:
+    project_id = cur_config["id_field_name"]
+    search_field_name = cur_config["search_field_name"]
+    if format == "select2":
+        result = {
+            "results": [],
+        }
+        for x in data["results"]:
+            item = {"id": f"#{x[project_id]}", "text": x[search_field_name]}
+            result["results"].append(item)
+
+        return result
+
+    elif format == "original":
+        return {"results": data}
+
+    else:
+        result = {"tc:suggestion": []}
+        for x in data["results"]:
+            item = {"tc:value": f"#{x[project_id]}", "tc:description": x[search_field_name]}
+            result["tc:suggestion"].append(item)
+
+        return result
