18126b7d80505e362cf4e80dee2c784093400ad4
csae8092 <peter.andorfer@oeaw.ac.at>
2024-03-20 11:36:53 +0100

    more tests

--- app/test_main.py
+++ app/test_main.py
@@ -20,5 +20,32 @@ def test_read_main():
 def test_correct_endpoint_number():
     with TestClient(app) as client:
         response = client.get("/")
-        endpoints = response.json()['endpoints']
+        endpoints = response.json()["endpoints"]
         assert len(endpoints) == len(DATABASES)
+
+
+def test_ac_endpoint():
+    db_id = "test"
+    endpoints = [key for key, _ in DATABASES[db_id]["endpoints"].items()]
+    for x in endpoints:
+        with TestClient(app) as client:
+            response = client.get(f"/ac/{db_id}/{x}")
+            assert response.status_code == 422
+            response = client.get(f"/ac/{db_id}/{x}?q=A")
+            assert response.status_code == 200
+
+
+def test_nonexiting_ac_endpoint():
+    with TestClient(app) as client:
+        response = client.get("/ac/quatsch/blödsinn?q=A")
+        assert response.status_code == 404
+
+
+def test_list_tables_endpoint():
+    db_id = "test"
+    with TestClient(app) as client:
+        response = client.get(f"/db/{db_id}/tables")
+        assert response.status_code == 200
+    with TestClient(app) as client:
+        response = client.get("/db/blödsinn/tables")
+        assert response.status_code == 404
